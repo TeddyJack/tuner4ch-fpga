98b032097b4b17fbef48fbd480bacf2f16628b23
Maximenkov <fapdayz@gmail.com>
2018-06-19 11:28:12 +0300

    Removed ASI output select. Now ASI = parallel

--- ts_muxer.v
+++ ts_muxer.v
@@ -19,14 +19,16 @@ output P_SYNC_OUT,
 
 output [7:0] DATA_OUT_ASI,	// TS output to ASI transmitter, contains one of the source streams reclocked to 27 MHz
 output DCLK_OUT_ASI,
-output reg D_VALID_OUT_ASI,
+output D_VALID_OUT_ASI,
 
 output [3:0] LEDS,
 output MISO
 );
 
-assign DCLK_OUT = !clk_27;
+assign DCLK_OUT = !clk_27;				// inversion of clock. not sure about it
 assign DCLK_OUT_ASI = DCLK_OUT;
+assign D_VALID_OUT_ASI = D_VALID_OUT;
+assign DATA_OUT_ASI = DATA_OUT;
 
 pll_for_ts_muxer pll_for_ts_muxer(
 .inclk0(CLK_IN),
@@ -113,47 +115,5 @@ wire [3:0] header_byte_addr;
 wire [7:0] header_byte;
 wire [7:0] data_to_miso;
 
-select_output select_output(	// this module chooses, which stream goes to ASI output
-.CLK(clk_27),
-.RST(RST),
-
-.SW(SW),
-
-.DATA_IN_BUS({DATA_OUT,DATA[23:0]}),
-.DCLK_BUS({clk_27,DCLK[2:0]}),
-.D_VALID_BUS({D_VALID_OUT,D_VALID[2:0]}),
-
-.DATA_OUT(data_from_selector),
-.DCLK_OUT(dclk_from_selector),
-.D_VALID_OUT(d_valid_from_selector),
-
-.RESET_ON_CHANGE_OUT(reset_on_change_out)
-);
-wire [7:0] data_from_selector;
-wire dclk_from_selector;
-wire d_valid_from_selector;
-wire p_sync_from_selector;
-wire reset_on_change_out;
-
-out_fifo_asi out_fifo_asi(
-.aclr((!RST) || (reset_on_change_out)),		// not sure we need this extra reset
-.data(data_from_selector),
-.rdclk(clk_27),
-.rdreq(!fifo_asi_empty),
-.wrclk(dclk_from_selector),
-.wrreq(d_valid_from_selector),
-.q(DATA_OUT_ASI),
-.rdempty(fifo_asi_empty)
-);
-wire fifo_asi_empty;
-
-
-always@(posedge clk_27 or negedge RST)
-begin
-if(!RST)
-	D_VALID_OUT_ASI <= 0;
-else
-	D_VALID_OUT_ASI <= !fifo_asi_empty;
-end
 
 endmodule
